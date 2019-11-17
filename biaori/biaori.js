f1bd4720d5ce8ec48bff5088f8562edfb32121c2
Yesterday17 <t@yesterday17.cn>
2019-11-17 19:38:08 +0800

    [biaori] finish script

--- biaori/biaori.js
+++ biaori/biaori.js
@@ -1,44 +1,42 @@
-let url = $request.url;
-let body = $request.body;
+let body = $response.body;
 
 const result = {};
 
-if (url && body) {
+if (body) {
   const json = JSON.parse(body);
-  if (json.action === "GetActivateBookList") {
-    result.response = { $status: 200 }
-    result.response.$body = JSON.stringify({
-      status: 0,
+  if (json.message === "获取图书列表成功") {
+    result.body = JSON.stringify({
+      message: json.message,
+      status: json.status,
       data: [
         {
           book_title: "标准日本语初级",
-          activation_date: "2016-02-21 22:40",
+          activation_date: "2000-01-01 00:00",
           activation_status: 2,
           activation_code: "000000000000000000",
           book_id: 10001
         },
         {
           book_title: "标准日本语中级",
-          activation_date: "2016-02-21 22:40",
+          activation_date: "2000-01-01 00:00",
           activation_status: 2,
           activation_code: "000000000000000000",
           book_id: 10002
         },
         {
           book_title: "标准日本语高级",
-          activation_date: "2016-02-21 22:40",
+          activation_date: "2000-01-01 00:00",
           activation_status: 2,
           activation_code: "000000000000000000",
           book_id: 10003
         }
       ]
     });
-  } else if (json.action === "advertisement") {
-    result.response = { $status: 200 }
-    result.response.$body = JSON.stringify({
-      status: "1",
+  } else if (json.message === "广告图片获取成功") {
+    result.body = JSON.stringify({
+      message: json.message,
+      status: json.status,
       data: [],
-      message: "广告图片获取成功",
       count: 0
     });
   }
