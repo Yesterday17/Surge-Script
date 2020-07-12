3529b6ad2ab21521578c4a59e675940da43a475e
Yesterday17 <t@yesterday17.cn>
2020-07-12 11:19:27 +0800

    fix(bilibili): bangumi download

--- bilibili/bangumi_download.js
+++ bilibili/bangumi_download.js
@@ -1,11 +1,19 @@
 const body = $response.body;
 const result = {};
 
+function toDownload(t) {
+  t.rights.allow_download = 1;
+}
+
 if (body) {
   try {
     const resp = JSON.parse(body);
     resp.result.rights.can_watch = 1;
-    resp.result.rights.allow_download = 1;
+    toDownload(resp.result);
+
+    if (Array.isArray(resp.result.episodes)) {
+      resp.result.episodes.forEach((t) => toDownload(t));
+    }
     result.body = JSON.stringify(resp);
   } catch (e) {
     console.log(e);
